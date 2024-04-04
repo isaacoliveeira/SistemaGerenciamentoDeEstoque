eb8bb3e29c1d34bf4b829a8994a81c1a766fb884
Isaac Alves Leal de Oliveira <isaacalves2406@gmail.com>
2024-04-04 15:56:03 -0300

    Atualizando

--- src/main/java/org/example/Controller/ProdutoRepository.java
+++ src/main/java/org/example/Controller/ProdutoRepository.java
@@ -5,11 +5,11 @@ import org.example.Models.Produto;
 import java.util.List;
 
 public class ProdutoRepository {
-    private List<Produto> carregarProdutos(String empresa) {
+    public static List<Produto> carregarProdutos(String empresa) {
         return null;
     }
 
-    private void salvarProdutos() {
+    public void salvarProdutos() {
 
     }
 }
